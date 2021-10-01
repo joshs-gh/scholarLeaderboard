67baf23b9c2a23dc054e9b644d4f63f47badda44
jsiegel-cn <joshsaddress@gmail.com>
2021-09-30 22:36:40 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useMoralis } from "react-moralis";
+import { useState } from "react";
 
 function App() {
+  const { authenticate, isAuthenticated, user } = useMoralis();
+  const [scholarArray, setScholarArray] = useState([]);
+
+  const setData = () => {
+    user.set("scholarArray", [
+      {
+        name: "Bob",
+        ronin: "ronin:328492308493028439243902",
+      },
+    ]);
+    user.save();
+    console.log(user.get("scholarArray"));
+  };
+
+  const addData = () => {
+    user.addUnique("scholarArray", {
+      name: "Pam",
+      ronin: "ronin:7897987897878",
+    });
+    user.save();
+    console.log(user.get("scholarArray"));
+  };
+
+  const delData = () => {
+    user.remove("scholarArray", {
+      name: "Pam",
+      ronin: "ronin:7897987897878",
+    });
+    user.save();
+    console.log(user.get("scholarArray"));
+  };
+
+  if (!isAuthenticated) {
+    return (
+      <div>
+        <button onClick={() => authenticate()}>Authenticate</button>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome {user.get("username")}</h1>
+      <button onClick={() => setData()}>Set Data</button>
+      <button onClick={() => addData()}>Add Data</button>
+      <button onClick={() => delData()}>Delete Data</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <MoralisProvider
+    appId="SEeyHCzG36isFXmvnR5rZwZ3234qhHmNJoAWRvF8"
+    serverUrl="https://qstqhij1mkyn.moralishost.com:2053/server"
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </MoralisProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
