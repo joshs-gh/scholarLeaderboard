8b3d6b1ddfc7b9e1c35c121b4e42b9395e6d010a
jsiegel-cn <joshsaddress@gmail.com>
2021-10-02 22:11:05 -0400

    maybe fucking working

--- src/App.js
+++ src/App.js
@@ -7,9 +7,15 @@ import { Input } from "@progress/kendo-react-inputs";
 const Moralis = require("moralis");
 
 function App() {
-  const SLPAPI = "https://api.lunaciaproxy.cloud/_earnings/";
-  const { authenticate, isAuthenticated, user, logout, isAuthenticating } =
-    useMoralis();
+  const SLPAPI = "https://axie-scho-tracker-server.herokuapp.com/api/account/"; // "https://api.lunaciaproxy.cloud/_earnings/";
+  const {
+    authenticate,
+    isAuthenticated,
+    user,
+    logout,
+    isAuthenticating,
+    setUserData,
+  } = useMoralis();
   const [scholarArray, setScholarArray] = useState([]);
   const [scholarCount, setScholarCount] = useState(0);
   const [visibleDialog, setVisibleDialog] = useState(false);
@@ -24,15 +30,6 @@ function App() {
   // console.log("URLDATA", urlData, error);
 
   useEffect(() => {
-    async function fetchSLP(ronin) {
-      try {
-        const response = await fetch(`${SLPAPI}${ronin}`);
-        const slp = await response.json();
-        return slp;
-      } catch (error) {
-        console.log("Fetch error: ", error);
-      }
-    }
     uid.length > 10 && setReadonly(true);
     let scholars = [];
     // console.log("USER", user);
@@ -49,33 +46,60 @@ function App() {
     }
     // console.log("SCHOLARS:", scholars);
     setScholarArray([...scholars]); // this creates a new array ref apparently
-    setScholarCount(scholars.length);
-    scholars.forEach((s) => {
-      fetchSLP(s.ronin).then((data) => {
-        s["slp"] = data.earnings.slp_inventory;
-        s["avgslp"] = Math.floor(
-          data.earnings.slp_inventory /
-            ((Date.now() / 1000 - data.earnings.last_claimed) / 86400)
-        );
-        setScholarArray(
-          scholars.sort((a, b) => (a.avgslp < b.avgslp ? 1 : -1))
-        );
-      });
-    });
+    refreshSLP(scholars);
   }, [data, user]);
+
+  async function fetchSLP(s) {
+    try {
+      const response = await fetch(`${SLPAPI}${s.ronin}`);
+      const slp = await response.json();
+      console.log(s.name, slp);
+      return slp;
+    } catch (error) {
+      console.log("Fetch error: ", error);
+    }
+  }
+
+  const refreshSLP = (scholars) => {
+    scholars.forEach((s) => {
+      fetchSLP(s)
+        .then((data) => {
+          console.log(s.name);
+          s["slp"] = data.slpData.gameSlp; // data.earnings.slp_inventory;
+          s["avgslp"] = Math.floor(
+            s.slp / ((Date.now() / 1000 - data.slpData.lastClaim) / 86400) // data.earnings.last_claimed) / 86400)
+          );
+        })
+        .finally(() => {
+          setScholarArray([
+            ...scholars.sort((a, b) => (a.avgslp < b.avgslp ? 1 : -1)),
+          ]);
+          setScholarCount(scholars.length);
+        });
+    });
+  };
 
   const toggleDialog = () => {
     setVisibleDialog(!visibleDialog);
   };
+
   const addScholar = () => {
     let scholar = {
       name: name.current.state.value,
       ronin: ronin.current.state.value,
     };
-    setScholarArray([...scholarArray, scholar]);
+    console.log("Adding Scholar: ", scholar);
     user.add("scholarArray", scholar);
     user.save();
+    setScholarArray([...scholarArray, scholar]);
     toggleDialog();
+    refreshSLP([...scholarArray, scholar]);
+  };
+
+  const resetDB = () => {
+    user.set("scholarArray", testscholars); // to reset for setToSortedString
+    user.save();
+    setScholarArray(testscholars);
   };
 
   if (!isAuthenticated && !readonly) {
@@ -92,8 +116,9 @@ function App() {
     <div>
       {scholarArray.map((s, id) => (
         <div key={id}>
-          {id + 1} / {s.name} / {s.ronin} / {s.slp ? s.slp : "Loading..."} /{" "}
-          {s.avgslp ? s.avgslp : "Loading..."}
+          {id + 1} / {s.name} / {s.ronin} /{" "}
+          {s.slp !== null ? s.slp : "Loading..."} /{" "}
+          {s.avgslp !== null ? s.avgslp : "Loading..."}
           <p />
         </div>
       ))}
@@ -107,6 +132,7 @@ function App() {
           <button onClick={() => logout()} disabled={isAuthenticating}>
             Logout
           </button>
+          <button onClick={resetDB}>Reset DB</button>
         </div>
       )}
       {visibleDialog && (
@@ -123,7 +149,7 @@ function App() {
           <p />
           <Input placeholder="Ronin Wallet" ref={ronin} />
           <DialogActionsBar>
-            <button className="k-button" onClick={addScholar}>
+            <button className="k-button" onClick={() => addScholar()}>
               Save
             </button>
             <button className="k-button" onClick={toggleDialog}>
@@ -137,6 +163,37 @@ function App() {
 }
 
 export default App;
+
+let testscholars = [
+  {
+    name: "Geraldine",
+    ronin: "ronin:82594247ad4d2f4a92067f2fbf1b5c8198893eb8",
+  },
+  {
+    name: "Godjell",
+    ronin: "ronin:6e64409eca843f94ae28a2780a4e6ce60f97d1cf",
+  },
+  {
+    name: "Test2",
+    ronin: "ronin:c91d96d21786eedca894f63a9f4509e2877bddeb",
+  },
+  {
+    name: "Bishop",
+    ronin: "ronin:7ac0d26639f2a8e90409a88781cde6c93ac6ef8b",
+  },
+  {
+    name: "Father",
+    ronin: "ronin:f2ed21c1d0be7635c96fccccad2eff5e9a4475e5",
+  },
+  // {
+  //   name: "cheese",
+  //   ronin: "ronin:a5c24437a76d9bf75d413f2234d10e6f1eea531c",
+  // },
+  // {
+  //   name: "Blessed",
+  //   ronin: "ronin:e7bc0a0dde843460d113376d671b1f9ca0fdb585",
+  // },
+];
 
 // const setData = () => {
 //   user.set("scholarArray", [
